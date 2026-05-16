7bcccdb7056ebdbf940c24d3a3bc019871e76d6e
ravi-fedram <ravikumar.fedram@gmail.com>
2026-05-16 16:49:50 +0530

    firts python program

---
+++ basic/01_lesson.py
@@ -0,0 +1,6 @@
+print("Welcome to python")
+
+def print_prompt(prompt):
+    print(prompt)
+
+print_prompt("Thanks python")
---
+++ basic/02_lesson.py
@@ -0,0 +1,3 @@
+import print_prompt from 01_lesson.py # type: ignore
+
+print_prompt("this is another example")
